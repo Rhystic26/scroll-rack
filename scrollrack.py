fc8ed7f629b4a541d9960924b4ea05904ed33fdc
rhystic26 <jacobjcasper@gmail.com>
2025-04-03 17:28:20 -0400

    Updated card data and image retrieval functions to take card names as parameters instead of HTTPS request strings.

--- scrollrack.py
+++ scrollrack.py
@@ -2,15 +2,25 @@ import requests, json
 from PIL import Image
 from io import BytesIO
 
-# Given a HTTPS request string, retrieves a card's data from scryfall in JSON format
-def retrieveCardData(cardRequest):
+# Given a card name, retrieves a card's data from scryfall in JSON format
+def retrieveCardData(cardName):
+    wordsInName = cardName.split()
+    cardRequest = "https://api.scryfall.com/cards/named?fuzzy="
+    for i in wordsInName:
+        cardRequest += i + "+"
     card = requests.get(cardRequest)
     cardJson = card.json()
     if (cardJson['object'] == "error"):
         return None
     return cardJson
-
-def retrieveCardImage(cardRequest):
+# https://api.scryfall.com/cards/named?fuzzy=aust+com
+# Given a card name, retrieves an image of a card from Scryfall
+def retrieveCardImage(cardName):
+    wordsInName = cardName.split()
+    cardRequest = "https://api.scryfall.com/cards/named?fuzzy="
+    for i in wordsInName:
+        cardRequest += i + "+"
+    cardRequest += "&format=image&version=normal"
     card = requests.get(cardRequest)
     cardImage = None
     if card.status_code == 200:
