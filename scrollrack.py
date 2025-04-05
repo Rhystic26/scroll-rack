8df1f29c52f9c63e99bc3807d48a2b04e4243fc1
rhystic26 <jacobjcasper@gmail.com>
2025-04-05 03:24:49 -0400

    Finished card analysis algorithm for single-face cards. Began work on multi-face card compatibility.

--- scrollrack.py
+++ scrollrack.py
@@ -53,22 +53,31 @@ def convertImageForGUI (cardImageData):
 class CardFace:
     def __init__(self, inputDict):
         self.name = inputDict['name']
+        #print(self.name)
         self.typeLine = inputDict['type_line']
         self.oracleText = []
-        self.Types = []
-        self.colors = inputDict['colors']
+        self.types = []
+        self.colors = [] 
+        if 'colors' in inputDict:
+            self.colors = inputDict['colors']
         self.manaCost = inputDict['mana_cost']
 
         self.typeLine = self.typeLine.split(" ")
         for i in self.typeLine:
             if i == "—":
                 break
-            self.Types.append(i)
+            self.types.append(i)
+        
+        for b, a in enumerate(self.types):
+            self.types[b] = a.lower()
         
         # Remove special characters from oracle text, split at newlines and periods
         self.tempOracleText = inputDict['oracle_text']
         self.tempOracleText = self.tempOracleText.replace("•", "")
+        self.tempOracleText = self.tempOracleText.replace("(", "")
+        self.tempOracleText = self.tempOracleText.replace(")", "")
         self.tempOracleText = self.tempOracleText.replace("—", "\n")
+        self.tempOracleText = self.tempOracleText.replace(":", "\n")
         if "Saga" in self.typeLine:
             self.tempOracleText = self.tempOracleText.replace("III ", "")
             self.tempOracleText = self.tempOracleText.replace("II ", "")
@@ -88,6 +97,21 @@ class CardFace:
             self.oracleText[b] = a.replace(self.name, "~")
         for b, a in enumerate(self.oracleText):
             self.oracleText[b] = a.replace(tempName[0], "~")
+        
+        for b, a in enumerate(self.oracleText):
+            self.oracleText[b] = a.lower()
+        
+        for i in reversed(self.oracleText):
+                if len(i) == 1:
+                    self.oracleText.remove(i)
+        
+        for b, a in enumerate(self.oracleText):
+            self.oracleText[b] = a.lstrip()
+
+        # Maybe change this section to deal with card face formatting better?
+        for i in reversed(self.oracleText):
+            if i.startswith('trample') or i.startswith('menace') or i.startswith('lifelink') or i.startswith('flying') or i.startswith('first strike') or i.startswith('double strike') or i.startswith('deathtouch') or i.startswith('haste') or i.startswith('hexproof') or i.startswith('indestructible') or i.startswith('ward') or i.startswith('protection') or i.startswith('vigilance') or i.startswith('reach'):
+                self.oracleText.remove(i)
 
         if (self.isCreature() or self.isVehicle()):
             self.power = inputDict['power']
@@ -95,13 +119,13 @@ class CardFace:
 
     # Checks if a given card face is a creature
     def isCreature(self):
-        for i in self.Types:
-            if i == 'Creature':
+        for i in self.types:
+            if i == 'creature':
                 return True
         return False
     
     def isVehicle(self):
-        if "Vehicle" in self.typeLine:
+        if "vehicle" in self.typeLine:
             return True
         return False
 
@@ -111,12 +135,17 @@ class Card:
         self.prices = jsonData['prices']
         self.priceUSD = self.prices['usd']
         self.cmc = jsonData['cmc']
+        self.cmc = int(self.cmc)
+        self.cmc = str(self.cmc)
         self.keywords = jsonData['keywords']
         self.layout = jsonData['layout']
         self.typeLine = jsonData['type_line']
-        self.Types = []
+        self.types = []
         self.colors = []
         self.cardFaces = None
+
+        for b, a in enumerate(self.keywords):
+            self.keywords[b] = a.lower()
 
         if self.layout == 'normal' or self.layout == 'prototype' or self.layout == 'mutate' or self.layout == 'meld' or self.layout == 'case' or self.layout == 'class':
             self.oracleText = []
@@ -124,14 +153,20 @@ class Card:
             for i in self.typeLine:
                 if i == "—":
                     break
-                self.Types.append(i)
+                self.types.append(i)
+            
+            for b, a in enumerate(self.types):
+                self.types[b] = a.lower()
 
             self.colors = jsonData['colors']
             
             # Remove special characters from oracle text, split at newlines and periods
             self.tempOracleText = jsonData['oracle_text']
             self.tempOracleText = self.tempOracleText.replace("•", "")
+            self.tempOracleText = self.tempOracleText.replace("(", "")
+            self.tempOracleText = self.tempOracleText.replace(")", "")
             self.tempOracleText = self.tempOracleText.replace("—", "\n")
+            self.tempOracleText = self.tempOracleText.replace(":", "\n")
             self.tempOracleText = self.tempOracleText.split('\n')
             for b, a in enumerate(self.tempOracleText):
                 self.tempOracleText[b] = a.split(".")
@@ -148,10 +183,24 @@ class Card:
             for b, a in enumerate(self.oracleText):
                 self.oracleText[b] = a.replace(tempName[0], "~")
 
+            for b, a in enumerate(self.oracleText):
+                self.oracleText[b] = a.lower()
+            
+            for i in reversed(self.oracleText):
+                if len(i) == 1:
+                    self.oracleText.remove(i)
+            
+            for b, a in enumerate(self.oracleText):
+                self.oracleText[b] = a.lstrip()
+
+            for i in reversed(self.oracleText):
+                if i.startswith('trample') or i.startswith('lifelink') or i.startswith('menace') or i.startswith('flying') or i.startswith('first strike') or i.startswith('double strike') or i.startswith('deathtouch') or i.startswith('haste') or i.startswith('hexproof') or i.startswith('indestructible') or i.startswith('ward') or i.startswith('protection') or i.startswith('vigilance') or i.startswith('reach'):
+                    self.oracleText.remove(i)
+
             if (self.isCreature() or self.isVehicle()):
                 self.power = jsonData['power']
                 self.toughness = jsonData['toughness']
-            print(self.oracleText)
+            #print(self.oracleText)
 
         elif self.layout == 'saga':
             self.oracleText = []
@@ -159,14 +208,20 @@ class Card:
             for i in self.typeLine:
                 if i == "—":
                     break
-                self.Types.append(i)
-
+                self.types.append(i)
+           
+            for b, a in enumerate(self.types):
+                self.types[b] = a.lower()
+          
             self.colors = jsonData['colors']
             
             # Remove special characters from oracle text, split at newlines and periods
             self.tempOracleText = jsonData['oracle_text']
             self.tempOracleText = self.tempOracleText.replace("•", "")
+            self.tempOracleText = self.tempOracleText.replace("(", "")
+            self.tempOracleText = self.tempOracleText.replace(")", "")
             self.tempOracleText = self.tempOracleText.replace("—", "\n")
+            self.tempOracleText = self.tempOracleText.replace(":", "\n")
             self.tempOracleText = self.tempOracleText.replace("III ", "")
             self.tempOracleText = self.tempOracleText.replace("II ", "")
             self.tempOracleText = self.tempOracleText.replace("I ", "")
@@ -176,7 +231,7 @@ class Card:
             for i in self.tempOracleText:
                 for g in i:
                     self.oracleText.append(g)
-            print(self.oracleText)
+            #print(self.oracleText)
             self.oracleText = [a for a in self.oracleText if a]
             
             tempName = self.name.split(",")
@@ -184,6 +239,20 @@ class Card:
                 self.oracleText[b] = a.replace(self.name, "~")
             for b, a in enumerate(self.oracleText):
                 self.oracleText[b] = a.replace(tempName[0], "~")
+            
+            for b, a in enumerate(self.oracleText):
+                self.oracleText[b] = a.lower()
+
+            for i in reversed(self.oracleText):
+                if len(i) == 1:
+                    self.oracleText.remove(i)
+            
+            for b, a in enumerate(self.oracleText):
+                self.oracleText[b] = a.lstrip()
+
+            for i in reversed(self.oracleText):
+                if i.startswith('trample') or i.startswith('lifelink') or i.startswith('menace') or i.startswith('flying') or i.startswith('first strike') or i.startswith('double strike') or i.startswith('deathtouch') or i.startswith('haste') or i.startswith('hexproof') or i.startswith('indestructible') or i.startswith('ward') or i.startswith('protection') or i.startswith('vigilance') or i.startswith('reach'):
+                    self.oracleText.remove(i)
 
             if (self.isCreature() or self.isVehicle()):
                 self.power = jsonData['power']
@@ -196,33 +265,81 @@ class Card:
 
     # Checks if a given card is a creature
     def isCreature(self):
-        for i in self.Types:
-            if i == 'Creature':
+        for i in self.types:
+            if i == 'creature':
                 return True
         return False
     
     def isVehicle(self):
-        if "Vehicle" in self.typeLine:
+        if "vehicle" in self.typeLine:
             return True
         return False
 
 # Given a card object, craft a Scryfall search query to find similar cards
 def analyzeCard (inputCard, mode):
-    searchString = ""
-    '''for i in inputCard.superTypes:
-        searchString += ("+t%3A" + i)
-    if inputCard.isCreature() and mode <= 1:
-        searchString += ("+pow>%3D" + inputCard.power + "+tou>%3D" + inputCard.toughness)'''
-    
-    return searchString
-    
+    searchString = "game%3Apaper"
 
-#evergreen keywords
-#evasion: trample, menace, flying
-#damage: first strike, double strike, deathtouch
-#haste: haste
-#defensive: hexproof, indestructible, ward
-#combat defensive: vigilance, reach
+    if inputCard.layout == 'flip' or inputCard.layout == 'transform' or inputCard.layout == 'adventure' or inputCard.layout == 'split' or inputCard.layout == 'modal_dfc':
+        pass
+
+    else:
+        searchString += "+%28"
+        for i in inputCard.oracleText:
+            x = i.replace(" ", "+")
+            searchString += ('+fulloracle%3A"' + x + '"+OR')
+        searchString += "+%29"
+
+        searchString += "+%28"
+        if inputCard.isCreature() and 'enchantment' in inputCard.types:
+            searchString += ("+t%3Acreature+OR" + "+t%3Aenchantment")
+        else:
+            for i in inputCard.types:
+                if i == 'legendary':
+                    continue
+                searchString += ("+t%3A" + i + "+OR")
+        searchString += "+%29"
+
+        if (inputCard.isCreature() or inputCard.isVehicle()):
+            if mode < 2:
+                searchString += "+%28"
+                searchString += ("+pow>%3D" + inputCard.power)
+                searchString += ("+tou>%3D" + inputCard.toughness)
+                searchString += "+%29"
+        
+        if mode < 4:
+            colorString = ""
+            for i in inputCard.colors:
+                colorString += i
+            searchString += "+%28"
+            searchString += ("+c<%3D" + colorString)
+            searchString += "+%29"
+        
+        if mode < 3:
+            searchString += "+%28"
+            searchString += ("+mv%3D" + inputCard.cmc)
+            searchString += "+%29"
+    if mode < 4:
+        searchString += "+%28"
+        if ('trample' in inputCard.keywords) or ('menace' in inputCard.keywords) or ('flying' in inputCard.keywords):
+            searchString += ("+%28" + "+kw%3Atrample+OR" + "+kw%3Amenace+OR" + "+kw%3Aflying" + "+%29")
+        if ('first strike' in inputCard.keywords) or ('double strike' in inputCard.keywords) or ('deathtouch' in inputCard.keywords):
+            searchString += ("+%28" + "+kw%3Afirststrike+OR" + "+kw%3Adoublestrike+OR" + "+kw%3Adeathtouch" + "+%29")
+        if ('haste' in inputCard.keywords):
+            searchString += ("+%28" + "+kw%3Ahaste" + "+%29")
+        if ('hexproof' in inputCard.keywords) or ('indestructible' in inputCard.keywords) or ('ward' in inputCard.keywords) or ('protection' in inputCard.keywords):
+            searchString += ("+%28" + "+kw%3Ahexproof+OR" + "+kw%3Aindestructible+OR" + "+kw%3Award+OR" + "+kw%3Aprotection" + "+%29")
+        if ('vigilance' in inputCard.keywords) or ('reach' in inputCard.keywords) or ('lifelink' in inputCard.keywords):
+            searchString += ("+%28" + "+kw%3Avigilance+OR" + "+kw%3Areach+OR" + "+kw%3Alifelink" + "+%29")
+        for i in inputCard.keywords:
+            print(i)
+            if i != 'trample' and i != 'menace' and i != 'flying' and i != 'first strike' and i != 'double strike' and i != 'deathtouch' and i != 'haste' and i != 'hexproof' and i != 'indestructible' and i != 'ward' and i != 'protection' and i != 'vigilance' and i != 'reach' and i != 'lifelink':
+                print("oops")
+                searchString += ("+kw%3A" + i)
+        searchString += "+%29"
+
+    searchString += "&order=usd&dir=asc"
+    print(searchString)
+    return searchString
 
 # ********************Main Program********************
 
@@ -284,9 +401,12 @@ while True:
         searchedCard = Card(searchedCardData)
         programWindow['displayInputCard'].update(data=searchedCardPNG)
         programWindow['cardName'].update(searchedCard.name)
-        programWindow['cardPrice'].update("Price: $" + searchedCard.priceUSD + " USD")
+        if searchedCard.priceUSD != None:
+            programWindow['cardPrice'].update("Price: $" + searchedCard.priceUSD + " USD")
         
     elif event == 'Run Scroll Rack':
+        suggestedCardList = None
+        cardDisplayList = []
 
         # Reset card name elements
         programWindow['suggestedCard1Name'].update("Card not found")
@@ -305,10 +425,17 @@ while True:
             if suggestedCardList == None:
                 continue
             else:
+                for i in reversed(suggestedCardList):
+                    if i.name == searchedCard.name:
+                        suggestedCardList.remove(i)
+                    for b in cardDisplayList:
+                        if i.name == b.name:
+                            suggestedCardList.remove(i)
                 x = 0
                 while len(cardDisplayList) < 3:
                     if 0 <= x < len(suggestedCardList):
                         cardDisplayList.append(suggestedCardList[x])
+                        x += 1
                     else:
                         break
             if len(cardDisplayList) == 3:
@@ -317,17 +444,20 @@ while True:
         # Display final suggested cards on screen
         if 0 < len(cardDisplayList):
             programWindow['suggestedCard1Name'].update(cardDisplayList[0].name)
-            programWindow['suggestedCard1Price'].update(cardDisplayList[0].priceUSD)
+            if cardDisplayList[0].priceUSD != None:
+                programWindow['suggestedCard1Price'].update("Price: $" + cardDisplayList[0].priceUSD + " USD")
             programWindow['displaySuggestedCard1'].update(convertImageForGUI(retrieveCardImage(cardDisplayList[0].name)))
 
         if 1 < len(cardDisplayList):
             programWindow['suggestedCard2Name'].update(cardDisplayList[1].name)
-            programWindow['suggestedCard2Price'].update(cardDisplayList[1].priceUSD)
+            if cardDisplayList[1].priceUSD != None:
+                programWindow['suggestedCard2Price'].update("Price: $" + cardDisplayList[1].priceUSD + " USD")
             programWindow['displaySuggestedCard2'].update(convertImageForGUI(retrieveCardImage(cardDisplayList[1].name)))
 
         if 2 < len(cardDisplayList):
             programWindow['suggestedCard3Name'].update(cardDisplayList[2].name)
-            programWindow['suggestedCard3Price'].update(cardDisplayList[2].priceUSD)
+            if cardDisplayList[2].priceUSD != None:
+                programWindow['suggestedCard3Price'].update("Price: $" + cardDisplayList[2].priceUSD + " USD")
             programWindow['displaySuggestedCard3'].update(convertImageForGUI(retrieveCardImage(cardDisplayList[2].name)))
 
                 
